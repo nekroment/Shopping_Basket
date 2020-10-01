becd44d9c51261bd39d48553278e64a43c088447
nekroment <adamcevs@gmail.com>
2020-10-01 08:27:39 +0300

    add App

--- front/src/App.js
+++ front/src/App.js
@@ -1,15 +1,16 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {connect} from 'react-redux';
 import {Route, Switch} from 'react-router-dom';
+import CartManager from './components/CartManager/CartManager.jsx';
+import { setBasketItemsThunkCreator, deleteBasketItemThunkCreator, changeBasketItemThunkCreator, changeBasketItemPlusThunkCreator } from './redux/reducer/basketReducer.js';
 
 function App(props) {
 
   useEffect(() => props.setBasket(), []);
-
   return (
     <div className="App">
       <Switch>
-        <Route path='/cart' component={() => <CartManager cost={props.cost} items={props.items} deleteItem={props.deleteItem} changeItem={props.changeItem}/>} />
+        <Route path='/cart' component={() => <CartManager focus={props.focus} changeItem={props.changeItem} cost={props.cost} items={props.items} deleteItem={props.deleteItem} />} />
       </Switch>
     </div>
   );
@@ -17,11 +18,12 @@ function App(props) {
 
 const mapStateToProps = (state) => ({
   items: state.basket.items,
-  cost: state.basket.cost
+  cost: state.basket.cost,
+  focus: state.basket.focus
 })
 
 export default connect(mapStateToProps, {
   setBasket: setBasketItemsThunkCreator,
   deleteItem: deleteBasketItemThunkCreator,
-  changeItem: changeBasketItemThunkCreator
+  changeItem: changeBasketItemThunkCreator,
 })(App);
