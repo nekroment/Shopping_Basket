a6697ed8275df83d4ad233ed20404109a403df4a
nekroment <adamcevs@gmail.com>
2020-10-02 02:25:04 +0300

    fix app module

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,12 +1,13 @@
 import { Module} from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CartModule } from './cart/cart.module';
+import { UserModule } from './user/user.module';
 require('dotenv/config');
 
 const connectionString = 'mongodb+srv://moonlight:382001@cluster0.zkmbj.mongodb.net/cart_shipping?retryWrites=true&w=majority';
 
 @Module({
-  imports: [MongooseModule.forRoot(connectionString), CartModule],
+  imports: [MongooseModule.forRoot(connectionString), CartModule, UserModule],
   controllers: [],
   providers: [],
 })
