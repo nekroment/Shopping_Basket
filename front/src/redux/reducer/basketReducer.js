2768b6bba36721e77c3ce849024e6d0477e3f81b
nekroment <adamcevs@gmail.com>
2020-10-01 08:26:56 +0300

    add basketReducer

--- front/src/redux/reducer/basketReducer.js
+++ front/src/redux/reducer/basketReducer.js
@@ -1,41 +1,71 @@
-
+import { basketAPI } from '../../api/api.js';
 
 const SET_BASKET_ITEMS = 'SET_BASKET_ITEMS';
 const SET_COST = 'SET_COST';
+const CHANGE_CART = 'CHANGE_CART';
+const DELETE_CART = 'DELETE_CART';
 
 let initialState = {
   items: [],
-  cost: 0
+  cost: 0,
+  focus: undefined
 }
 
 const basketReducer = (state = initialState, action) => {
   switch (action.type) {
     case SET_BASKET_ITEMS: {
-      let stateCopy = {...state};
+      let stateCopy = { ...state };
       stateCopy.items = [...action.items];
       return stateCopy;
     }
     case SET_COST: {
       let newCost = 0;
       for (let i = 0; i < state.items.length; i++) {
-        newCost += Number(state.items[i].number);
+        newCost += state.items[i].number * Number(state.items[i].price);
       }
-      return {...state, cost: newCost};
+      return { ...state, cost: +newCost.toFixed(2) };
+    }
+    case CHANGE_CART: {
+      let stateCopy = { ...state };
+      stateCopy.items = [...state.items];
+      if (action.isBt === 'plus') {
+        stateCopy.focus = undefined;
+      } else {
+        stateCopy.focus = action.id;
+      }
+      for (let i = 0; i < stateCopy.items.length; i++) {
+        if (stateCopy.items[i].id === action.id) {
+          stateCopy.items[i].number = action.number;
+        }
+      }
+      return stateCopy;
+    }
+    case DELETE_CART: {
+      let stateCopy = {...state};
+      stateCopy.items = [...state.items];
+      stateCopy.items = stateCopy.items.filter((item) => item.id != action.id);
+      return stateCopy;
     }
     default: return state;
   }
 }
 
-export const setCost = () => ({type: SET_COST});
-export const setBasketItems = (items) => ({type: SET_BASKET_ITEMS, items});
+export const deleteCart = (id) => ({type: DELETE_CART, id});
+export const changeCart = (id, number, isBt) => ({ type: CHANGE_CART, id, number, isBt });
+export const setCost = () => ({ type: SET_COST });
+export const setBasketItems = (items) => ({ type: SET_BASKET_ITEMS, items });
 
 export const setBasketItemsThunkCreator = () => {
   return async (dispatch) => {
     try {
       let responce = await basketAPI.getBasket();
-
-      let basketItems = await basketAPI.createBasket(responce.data);
-      dispatch(setBasketItems(basketItems.data));
+      let cart = responce.data.map(element => {
+        element.number = 1;
+        return element;
+      });
+      //let basketItems = await basketAPI.createBasket(responce.data);
+      //dispatch(setBasketItems(basketItems.data));
+      dispatch(setBasketItems(cart));
       dispatch(setCost());
     } catch (error) {
 
@@ -46,20 +76,24 @@ export const setBasketItemsThunkCreator = () => {
 export const deleteBasketItemThunkCreator = (id) => {
   return async (dispatch) => {
     try {
-      let responce = await basketAPI.deleteItem(id);
-      dispatch(setBasketItemsThunkCreator());
+      //let responce = await basketAPI.deleteItem(id);
+      //dispatch(setBasketItemsThunkCreator());
+      dispatch(deleteCart(id));
+      dispatch(setCost());
     } catch (error) {
 
     }
   }
 };
 
-export const changeBasketItemThunkCreator = (id, number) => {
+export const changeBasketItemThunkCreator = (id, number, isBt = '') => {
   return async (dispatch) => {
     try {
-      if(number < 50 && number => 0) {
-        let responce = await basketAPI.changeItem(id, number);
-        dispatch(setBasketItemsThunkCreator());
+      if (number <= 50 && number >= 0) {
+        //let responce = await basketAPI.changeItem(id, number);
+        //dispatch(setBasketItemsThunkCreator());
+        dispatch(changeCart(id, number, isBt));
+        dispatch(setCost());
       }
     } catch (error) {
 
