ca308a766e8590e0a76c31247015390348e6eb2f
nekroment <adamcevs@gmail.com>
2020-10-02 02:23:47 +0300

    change CartManager

--- front/src/components/CartManager/CartManager.jsx
+++ front/src/components/CartManager/CartManager.jsx
@@ -1,24 +1,23 @@
 import React from 'react';
 import Cart from '../Cart/Cart.jsx';
 import { NavLink } from 'react-router-dom';
+import './CartManager.css';
 
 const CartManager = (props) => {
 
   const createCarts = () => {
-    return props.items.map(item => <Cart focus={props.focus} key={item.id} item={item} deleteItem={props.deleteItem} changeItem={props.changeItem}/>)
+    return props.items.map(item => <Cart focus={props.focus} key={item.id} item={item} deleteItem={props.deleteItem} changeItem={props.changeItem} />)
   }
   const carts = createCarts();
   return (
     <>
-    <div>
-    {carts}
-
-    </div>
-    <div>
-      {props.cost}
-        <NavLink to='/shipping'><button>BUY</button></NavLink>
-      
-    </div>
+      <div>
+        {carts}
+      </div>
+      <div className={'buy'}>
+        <p className={"price-button"}>{props.cost + ' ' + '€'}</p>
+        <NavLink to='/shipping'><button className={'buy-button'}>BUY</button></NavLink>
+      </div>
     </>
   )
 };
