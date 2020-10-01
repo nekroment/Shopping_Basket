debce915998447fab7217df0c9e6a5cb38823ce8
nekroment <adamcevs@gmail.com>
2020-10-01 23:22:24 +0300

    add Cart module

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,8 +1,12 @@
-import { Module } from '@nestjs/common';
+import { Module} from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { CartModule } from './cart/cart.module';
+require('dotenv/config');
+
+const connectionString = 'mongodb+srv://moonlight:382001@cluster0.zkmbj.mongodb.net/cart_shipping?retryWrites=true&w=majority';
 
 @Module({
-  imports: [CartModule],
+  imports: [MongooseModule.forRoot(connectionString), CartModule],
   controllers: [],
   providers: [],
 })
